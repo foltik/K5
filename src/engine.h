5ea4e8af6fc79f03d4f7ad484d46b0ef3d46b3b8
Jack Foltz <jack@foltz.tech>
2016-11-02 09:23:21 -0400

    Remove pragma lib definitions in linux header

--- src/engine.h
+++ src/engine.h
@@ -1,9 +1,7 @@
 #pragma once
 #ifdef __linux__
 #include "SDL2/SDL.h"
-#pragma comment(lib, "SDL2.lib")
 #include "SDL2/SDL_ttf.h"
-#pragma comment(lib, "SDL2_ttf.lib")
 
 #elif _WIN32
 #include <SDL2/SDL.h>
