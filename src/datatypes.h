b0699ccca336fba4dd36403c714d6b6f0c32b4d9
Jack Foltz <jack@foltz.me>
2017-04-29 10:24:11 -0400

    Remove datatypes and switch to GLM

--- src/datatypes.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "mat4f.h"
-#include "quat.h"
-
-#include "vec2f.h"
-#include "vec3f.h"
-
-#include "mesh.h"
-#include "vertex3f.h"
--- src/vertex3f.cpp
+++
@@ -1,5 +0,0 @@
-#include "vertex3f.h"
-
-vertex3f::vertex3f(vec3f POS) {
-	pos = POS;
-}
