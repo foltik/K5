b83c2093ad91e5667fdc4d0361a7a09238c4f4a0
Jack Foltz <jack@foltz.me>
2017-05-11 08:50:11 -0400

    Fix bad sizeof preventing models from rendering correctly \(aha\)

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -23,7 +23,7 @@ void Mesh::genMesh() {
 	glBindVertexArray(vao);
 	
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, (GLsizei)verticies.size() * sizeof(GLuint), &verticies[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, (GLsizei)verticies.size() * sizeof(Vertex), &verticies[0], GL_STATIC_DRAW);
 	
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, (GLsizei)indicies.size() * sizeof(GLuint), &indicies[0], GL_STATIC_DRAW);
@@ -32,10 +32,10 @@ void Mesh::genMesh() {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)0);
 
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, Vertex::norm));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, norm));
 
 	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, Vertex::tex));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, tex));
 
 	glBindVertexArray(0);
 }
