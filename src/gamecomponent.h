dac25b0a3db4af8d4091031cfbc5185e914ae456
Jack <jfoltzt@gmail.com>
2017-09-07 15:29:42 -0400

    Getters/setters instead of protected pointer, make polymorphic methods pure virtual

--- src/gamecomponent.h
+++ src/gamecomponent.h
@@ -1,13 +1,24 @@
 #pragma once
+#include <cassert>
 
 class GameObject;
 
 class GameComponent {
 public:
-    virtual void Input() {};
-    virtual void Update() {};
-    virtual void Render() {};
+    void SetParent(GameObject& parent_) {
+        assert(parent == nullptr);
+        parent = &parent_;
+    }
 
-protected:
+    GameObject& GetParent() {
+        assert(parent != nullptr);
+        return *parent;
+    }
+
+    virtual void Input(bool keyboard[512], bool mouse[16], float mouseX, float mouseY) = 0;
+    virtual void Update() = 0;
+    virtual void Render() = 0;
+
+private:
     GameObject* parent = nullptr;
 };
