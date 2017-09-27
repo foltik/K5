fd2a023e43ee768194d1c44508f006c803d4641f
Jack <jfoltzt@gmail.com>
2017-09-27 15:22:00 -0400

    Add base fields to light class

--- src/light.cpp
+++ src/light.cpp
@@ -1,3 +1,36 @@
 #include "light.h"
 
+#include "shader.h"
+
+Light::Light(glm::vec3 _color, float _intensity) : color(_color) {
+    intensity = _intensity;
+}
+
+Light::~Light() {
+    delete shader;
+}
+
+const glm::vec3 &Light::getColor() const {
+    return color;
+}
+
+void Light::setColor(const glm::vec3 &color) {
+    Light::color = color;
+}
+
+float Light::getIntensity() const {
+    return intensity;
+}
+
+void Light::setIntensity(float intensity) {
+    Light::intensity = intensity;
+}
+
+Shader *Light::getShader() const {
+    return shader;
+}
+
+void Light::setShader(Shader *shader) {
+    Light::shader = shader;
+}
 
--- src/light.h
+++ src/light.h
@@ -4,5 +4,24 @@
 #include "gamecomponent.h"
 
 class Light : public GameComponent {
+public:
+    Light(glm::vec3 _color, float _intensity);
+    ~Light();
+
+private:
     glm::vec3 color;
+public:
+    const glm::vec3 &getColor() const;
+    void setColor(const glm::vec3 &color);
+
+    float getIntensity() const;
+    void setIntensity(float intensity);
+
+    Shader *getShader() const;
+    void setShader(Shader *shader);
+
+private:
+    float intensity;
+
+    Shader* shader;
 };
